cd3ce71bc4cb8b94f37637b950f98868492b02d5
Björnar Schinkel <bjornar.schinkel@etu.umontpellier.fr>
2025-05-06 17:32:19 +0200

    updated some parsing errors

--- ConsoleApp1/Engine/Scripts/MathLibrary/Parse.cs
+++ ConsoleApp1/Engine/Scripts/MathLibrary/Parse.cs
@@ -6,14 +6,14 @@ public static class Parse
     public static Vector4 Vec4(string str)
     {
         str = str.Trim(['(',')']);
-        string[] parts = str.Split(',');
+        string[] parts = str.Split([',','.'], StringSplitOptions.RemoveEmptyEntries);
         if (parts.Length != 4)
             return Vector4.Zero;
 
-        float x = float.Parse(parts[0].Trim(), CultureInfo.InvariantCulture);
-        float y = float.Parse(parts[1].Trim(), CultureInfo.InvariantCulture);
-        float z = float.Parse(parts[2].Trim(), CultureInfo.InvariantCulture);
-        float w = float.Parse(parts[3].Trim(), CultureInfo.InvariantCulture);
+        float x = Float(parts[0].Trim());
+        float y = Float(parts[1].Trim());
+        float z = Float(parts[2].Trim());
+        float w = Float(parts[3].Trim());
 
         return new Vector4(x, y, z, w);
     }
@@ -25,9 +25,9 @@ public static class Parse
         if (parts.Length != 3)
             return Vector3.Zero;
 
-        float x = float.Parse(parts[0].Trim(), CultureInfo.InvariantCulture);
-        float y = float.Parse(parts[1].Trim(), CultureInfo.InvariantCulture);
-        float z = float.Parse(parts[2].Trim(), CultureInfo.InvariantCulture);
+        float x = Float(parts[0].Trim());
+        float y = Float(parts[1].Trim());
+        float z = Float(parts[2].Trim());
 
         return new Vector3(x, y, z);
     }
@@ -39,8 +39,8 @@ public static class Parse
         if (parts.Length != 2)
             return Vector2.Zero;
 
-        float x = float.Parse(parts[0].Trim(), CultureInfo.InvariantCulture);
-        float y = float.Parse(parts[1].Trim(), CultureInfo.InvariantCulture);
+        float x = Float(parts[0].Trim());
+        float y = Float(parts[1].Trim());
 
         return new Vector2(x, y);
     }
@@ -48,6 +48,6 @@ public static class Parse
     public static float Float(string str)
     {
         str = str.Trim(['(',')']);
-        return float.Parse(str);
+        return Float(str);
     }
 }
